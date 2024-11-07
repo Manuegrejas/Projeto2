13e33d536060b59dbec6d7c2872fdfd96f2207c6
Manuegrejas <142542958+Manuegrejas@users.noreply.github.com>
2024-11-07 01:30:03 +0000

    3° etapa

--- Estoque.cpp
+++ Estoque.cpp
@@ -7,16 +7,72 @@ using namespace std;
 
 class Estoque{
     private:
+        Produto* produto[100];
+        Alimento*ali;
+        Eletronico*ele;
+        Limpeza*lim;
 
     public:
-        Produto* produto[10];
+        Estoque(){}
 
     void VisualizarEstoque(){
 
     }
 
-    void AdicionarProduto(){
+    int i = 0;
 
+    void AdicionarProduto(){
+        string secao;
+        string nome;
+        float preco;
+        int quantidade;
+        string DataDeValidade;
+        string tipo;
+        float TensaoDeOperacao;
+        string DataDeFabricacao;
+        string marca;
+        string categoria;
+        cout << "Qual seção do produto é?\n a - Alimento\n e - Eletrônico\n l - Limpeza" << endl;
+        cin >> secao;
+        if (secao == "a"){
+            cout << "Nome: ";
+            cin >> nome; 
+            cout << "\nPreço: ";
+            cin >> preco;
+            cout << "\nQuatidade: ";
+            cin >> quantidade;
+            cout << "\nData de validade: ";
+            cin >> DataDeValidade;
+            cout << "\nTipo de Alimento: ";
+            cin >> tipo;
+        produto[i] = new Alimento(nome, preco, quantidade, DataDeValidade, tipo);
+        } else if (secao == "e"){
+            cout << "Nome: ";
+            cin >> nome; 
+            cout << "\nPreço: ";
+            cin >> preco;
+            cout << "\nQuatidade: ";
+            cin >> quantidade;
+            cout << "\nTensão de Operação: ";
+            cin >> TensaoDeOperacao; 
+            cout << "\nData de Fabricação: ";
+            cin >> DataDeFabricacao;
+        produto[i] = new Eletronico(nome, preco, quantidade, TensaoDeOperacao, DataDeFabricacao);
+        } else if (secao == "l"){
+            cout << "Nome: ";
+            cin >> nome; 
+            cout << "\nPreço: ";
+            cin >> preco;
+            cout << "\nQuatidade: ";
+            cin >> quantidade;
+            cout << "\nMarca: ";
+            cin >> marca;
+            cout << "\nCategoria de risco: ";
+            cin >> categoria;
+        produto[i] = new Limpeza(nome, preco, quantidade, marca, categoria);
+        }
+
+        i = i++;
     }
 
     void EditarQuantidade(){
@@ -35,8 +91,9 @@ class Estoque{
 
     }
 
-    void Sair(){
-        
+    int Sair(){
+        return 0; 
+        //adicionar os dados pro arquivo
     }
 
 };
--- main.cpp
+++ main.cpp
@@ -11,21 +11,49 @@
 using namespace std;
 
 int main(){
+
+    Estoque e1;
+
+    int n = 1;
     char x;
-    while (1){
+    while (n != 0){
         cout << "\n=========== MENU ===========" << endl;
         cout << "Opções:\n" << endl;
-        cout << "   VISUALIZAR ESTOQUE   -> e" << endl;
+        cout << "  VISUALIZAR ESTOQUE   -> e" << endl;
         cout << "   ADICIONAR PRODUTO   -> a" << endl;
         cout << "   EDITAR QUANTIDADE   -> q" << endl;
         cout << "   PESQUISAR PRODUTO   -> p" << endl;
-        cout << "   VENDA DE PRODUTO   -> v" << endl;
+        cout << "    VENDA DE PRODUTO   -> v" << endl;
         cout << "   VISUALIZAR RECIBO   -> r" << endl;
-        cout << "        SAIR            -> s" << endl;
+        cout << "                SAIR   -> s" << endl;
         cout << "============================" << endl;
 
         cout << "Digite a letra correspondente a função que será realizada: " << endl;
-        scanf(" %c", &x);
+        cin >> x;
+
+        switch (x){
+            case 'e':
+                e1.VisualizarEstoque();
+                break;
+            case 'a':
+                e1.AdicionarProduto();
+                break;
+            case 'q':
+                e1.EditarQuantidade();
+                break;
+            case 'p':
+                e1.PesquisarProduto();
+                break;
+            case 'v':
+                e1.VendaProduto();
+                break;
+            case 'r':
+                e1.VisualizarRecibo();
+                break;
+            case 's':
+                n = e1.Sair();
+                break; 
+        }
 }
     return 0;
 }
